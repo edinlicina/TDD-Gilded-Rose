d71e5a8c87062e958ea9bec435f55099a17e8b1b
Edin <edin.licina.2@gmail.com>
2024-06-18 16:09:03 +0200

    tested first requirements

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,24 @@
-describe("Gilded Rose", function() {
-
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
+describe("Gilded Rose", function () {
+  it("should pass", function () {
+    expect(true).toEqual(true);
   });
 
+  it("should increase quality of Aged brie the older it gets", function () {
+    items = [new Item("Aged Brie", 2, 0)];
+    update_quality();
+    expect(items[0].name).toEqual("Aged Brie");
+    expect(items[0].quality).toEqual(1);
+    expect(items[0].sell_in).toEqual(1);
+  });
+  it("should degrade quality twice as fast after sell by date has passed", function () {
+    items = [new Item("Elixir of Mongoose", 0, 7)];
+    update_quality();
+    expect(items[0].name).toEqual("Elixir of Mongoose");
+    expect(items[0].quality).toEqual(5);
+  });
+  it("should never be a negative quality", function () {
+    items = [new Item("Elixir of Mongoose", 5, 0)];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+  });
 });
